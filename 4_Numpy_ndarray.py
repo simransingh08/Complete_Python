cb8b6a9f147f3b38851a14594d50fd63ec1b1114
simransin <simransingh9876rajgir@gmail.com>
2023-11-08 15:53:16 +0530

    ndarray

---
+++ 4_Numpy_ndarray.py
@@ -0,0 +1,8 @@
+import numpy as np 
+# with the help of ndarray we can create 2D and 3D array
+
+arr = np.array([[1,2,3,4], [5,6,7,8]]); #this is 2D array
+print (arr);
+
+arr1 = np.array ([[1,2,3,4],[11,12,13,14],[111,222,333,444]]);# this is 3Darray
+print(arr1);
