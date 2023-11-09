5f05db43f1250f7cbf2e65e362c099aae34cc861
simransin <simransingh9876rajgir@gmail.com>
2023-11-09 10:41:20 +0530

    dtype with different types

--- 5_NP_datatype.py
+++ 5_NP_datatype.py
@@ -3,3 +3,12 @@ import numpy as np
 
 arr = np.array([1,2,3,4,5]);
 print(arr.dtype);
+
+int_arr = np.array([-3,-2,0,1]);
+print(int_arr.dtype);
+
+float_arr = np.array([0.1,0.2,0.3]);
+print(float_arr.dtype);
+
+complex_arr = np.array([1+2j,2+3j,3+4j]);
+print(complex_arr.dtype);
